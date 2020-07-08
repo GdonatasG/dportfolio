4763e8bf58c7656929ef0d285c757e0a39f2e664
GdonatasG <zitkusd@gmail.com>
2020-07-08 15:47:54 +0300

    final version

--- lib/pages/portfolio/portfolio_data.dart
+++ lib/pages/portfolio/portfolio_data.dart
@@ -37,7 +37,7 @@ const List<PortfolioItem> portfolioItems = [
         "Mobile",
         "Android",
 
-        /*"iOS"*/
+        //*"iOS"*/
       ],
       coverUrl: "assets/images/portfolio/dportfolio/dportfolio_cover.png",
       listOfImagesUrl: [
